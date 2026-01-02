4f489b2da3fe69f7fb323bcc57811733f9c11352
Dongseup <dongseup83@gmail.com>
2026-01-02 19:40:18 +0900

    11단계: AppModule에 등록 - src/app.module.ts에 AuthModule import 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-
     DatabaseModule,
+    AuthModule, // 추가
   ],
   controllers: [AppController],
   providers: [AppService],
